c446126e55405327712513aaf50f8fb5b0fb748b
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-09-22 22:35:54 -0400

    Create displayEmployeeShifts function

--- cc_6.js
+++ cc_6.js
@@ -39,9 +39,36 @@ console.log(employees);
 //Task 2 display employee shifts with the day and hours they worked
 function displayEmployeeShifts(employee) {
     console.log(`Schedule for ${employee.name}`);
+//Iterates each day and hours that the selected employee is supposed to work
     employee.shifts.forEach(shift => {
         console.log(`Day: ${shift.day}, Hours: ${shift.hours}`);
     });
 }
 //Shows the employee shift for the 2nd value in the array, for "Sander"
 displayEmployeeShifts(employees[1]);
+
+//Task 3 create a function that assigns new shifts to employees
+function assignShift(employeeName, day, hours){
+//Creates variable worker to reference the employee arry
+    let worker = employees.find(worker => worker.name === employeeName);
+//If the given name is in the array, will run the following if-else statement
+    if (worker){
+//Makes sure that the employee is not already working that day
+        let alreadyWorking = worker.shifts.find(shift => shift.day === day);
+//logs that employee is working that day and cannot work the assigned shift
+        if (alreadyWorking){
+            console.log(`${worker.name} cannot work, already on schedule for the day`);
+        }
+//uses the push command to push in the newly assigned shift at the end of that employees shifts array
+        else{
+            worker.shifts.push({day: day, hours: hours})
+            console.log(`${worker.name} works ${hours} hours on ${day} now`)
+    }}
+//Pops out an error message saying that the referenced employee isnt in the employees array
+    else{
+            console.log(`${employeName} not found`);
+    }
+}
+//Checks to see that the function works correctly
+assignShift("Jorge","Th",5);
+console.log(employees[2]);
