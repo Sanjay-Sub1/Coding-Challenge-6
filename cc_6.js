8161bca46173e0159b4c79ca316c59afb4ea6b5b
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-09-22 21:59:40 -0400

    Initialize employees with shift schedules

--- cc_6.js
+++ cc_6.js
@@ -1,1 +1,37 @@
+//Task 1 create an Array for the employees, including the days and they worked and amount of hours they worked on said day
+let employees = [
+    {
+        name: "Sanjay",
+        shifts: [
+            { day: "M", hours: 4 },
+            { day: "W", hours: 6 },
+            { day: "F", hours: 10 }
+        ]
+    },
+    {
+        name: "Sander",
+        shifts: [
+            { day: "T", hours: 8 },
+            { day: "Th", hours: 6 },
+            { day: "St", hours: 12 }
+        ]
+    },
+    {
+        name: "Jorge",
+        shifts: [
+            { day: "M", hours: 6 },
+            { day: "T", hours: 5 },
+            { day: "St", hours: 12 }
+        ]
+    },
+    {
+        name: "Charles",
+        shifts: [
+            { day: "W", hours: 2 },
+            { day: "Th", hours: 5 },
+            { day: "Sn", hours: 12 }
+        ]
+    }
+];
 
+console.log(employees);
